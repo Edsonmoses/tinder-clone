b575ccc954a366b8b76339903b534e544b0bdff2
Edson Wandera <edsonwandera@yahoo.com>
2023-03-29 19:24:11 +0300

    added client and server folder

---
+++ client/src/pages/Home.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Nav from '../components/Nav'
+import AuthModal from "../components/AuthModal"
+import {useState} from 'react'
+
+const Home = () => {
+    const [showModal, setShowModal] = useState(false)
+    const [isSignUp, setIsSignUp] = useState(true)
+    const authToken = false
+    const handleClick = () =>{
+        console.log('clicked')
+        setShowModal(true)
+        setIsSignUp(true)
+    }
+  return (
+    <div className="overlay">
+    <Nav
+                authToken={authToken}
+                minimal={false}
+                setShowModal={setShowModal}
+                showModal={showModal}
+                setIsSignUp={setIsSignUp}
+            />
+
+    <div className="home">
+        <h1 className="primary-title">Swipe Right®</h1>
+        <button className='primary-button' onClick={handleClick}>
+            {authToken ? 'Signout' : 'Create Account'}
+        </button>
+
+        {showModal && (
+                    <AuthModal setShowModal={setShowModal} isSignUp={isSignUp}/>
+                )}
+    </div>
+  </div>
+  )
+}
+
+export default Home
